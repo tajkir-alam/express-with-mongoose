db61974482e23b9ac7f284e1f43e5fd814a7361d
MD Tajkir Alam Rion <tajkir.alam@gmail.com>
2024-05-22 22:25:13 +0600

    api documentation added on the home route

--- src/app.ts
+++ src/app.ts
@@ -7,11 +7,42 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
+// All routes are here
 app.use("/api/products", ProductRoutes);
 app.use("/api/orders", OrderRoutes);
 
+// Home route
 app.get("/", (req: Request, res: Response) => {
-  res.send("hello world!!!");
+  // res.status(200).json({
+  //   "Get All Product": "www.fdfd.com"
+  // });
+  res.status(200).send(
+    `
+    <h1 style="text-align: center;">API Documentation</h1>
+    <ul style="list-style: decimal; text-align: center;">
+      <li>
+        <a target="_blank" href="https://e-commerce-mongoose-ts.vercel.app/api/products">Get All Products</a>
+      </li>
+      <br />
+      <li>
+        <a target="_blank" href="https://e-commerce-mongoose-ts.vercel.app/api/products/:productID">Get Product By ID</a>
+      </li>
+      <br />
+      <li>
+        <a target="_blank" href="https://e-commerce-mongoose-ts.vercel.app/api/products?searchTerm=iphone">Search Product</a>
+      </li>
+      <br />
+      <li>
+        <a target="_blank" href="https://e-commerce-mongoose-ts.vercel.app/api/orders">Get All Orders</a>
+      </li>
+      <br/>
+      <li>
+        <a target="_blank" href="https://e-commerce-mongoose-ts.vercel.app/api/orders?email=level2@programming-hero.com">Get All Orders By Email</a>
+      </li>
+      <br/>
+    </ul>
+    `
+  );
 });
 
 app.use((req: Request, res: Response) => {
